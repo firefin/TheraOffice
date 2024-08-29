97a4bd6e1f92a9a166f776ad3e089e3ec43cddd6
firefin <christianakd@live.com>
2024-08-28 21:13:19 -0400

    Added basic functionality.

--- ProgramAssign1/Program.cs
+++ ProgramAssign1/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace ProgramAssign1
+﻿using System.Globalization;
+
+namespace ProgramAssign1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            List<Patient> patients = new List<Patient>();
+            List<Physician> physicians = new List<Physician>();
+
+
+            Patient p = new Patient();
+            Console.WriteLine("Enter Patient Info:");
             
+            Console.Write("Name: ");
+            p.Name = Console.ReadLine();
+
+            Console.Write("Age: ");
+            Int32.TryParse(Console.ReadLine(), out int age);//obviously doesn't check for invalid input.
+            p.Age = age;
+            
+            Console.Write("Address: ");
+            p.Address = Console.ReadLine();
+
+            Console.Write("Race: ");
+            p.Race = Console.ReadLine();
+
+            Console.Write("Gender: ");
+            p.Gender = Console.ReadLine();
+
+            Console.Write("Date of Birth (MM-DD-YYYY): ");
+            p.Birthdate = DateTime.ParseExact(Console.ReadLine(), "MM-dd-yyyy", CultureInfo.InvariantCulture);//doesn't check for invalid input
+
+
+            Console.WriteLine(p);
+
+
         }
     }
 
@@ -14,22 +45,29 @@
 
     public class Patient
     {
-        public required string Name { get; set; }
-        public required string Address { get; set; }
-        public required string Race {  get; set; }
-        public required string Gender { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Race {  get; set; }
+        public string Gender { get; set; }
         public DateTime Birthdate { get; set; }
         public int Age { get; set; }
         public List<MedicalNote> MedicalNotes { get; set; } = new List<MedicalNote>();
-                                                          
+
         //Logic missing 
-        
+
+
+        //ToString overload
+        public override string ToString()
+        {
+            return $"Name: {Name}\nAge: {Age}";//missing others
+        }
+
     }
 
     public class Physician
     {
-        public required string Name { get; set; }
-        public required string LicenseNumber { get; set; }
+        public string Name { get; set; }
+        public string LicenseNumber { get; set; }
         public DateTime GraduationDate { get; set; }
         public List<string>? Specializations { get; set; }
 
